2bead5e16f5fc9f16fcc823cb48da661d7ae0dcf
Xemmas <xemmas390@gmail.com>
2023-05-25 18:22:03 +1000

    Test Branch
    Test

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public float dashSpeed = 15.0f;
+    public float dashTime = 0.2f;
+    public float dashCooldown = 2.0f;
+    private float nextDash = 0.0f;
+    private bool isDashing = false; 
     public Rigidbody2D rb;
     Vector2 movement;
     Vector2 mousePos;
@@ -15,8 +20,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!isDashing)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
@@ -31,15 +39,44 @@ public class PlayerMovement : MonoBehaviour
             transform.position=new Vector2(transform.position.x,transform.position.y + distance); 
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.LeftControl) && Time.time > nextDash)
+    {
+        StartCoroutine(Dash());
+        GetComponent<PlayerHealth>().AreaAttack(); // perform the area attack
+        nextDash = Time.time + dashCooldown; // update the nextDash time
+    }
+
+    
     }
 
     void FixedUpdate()
     {
+        if (!isDashing)
+        {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
         Vector2 lookDir = mousePos - rb.position;   
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; 
         rb.rotation = angle;
+        }
     }
+
+     IEnumerator Dash()
+{
+    float startTime = Time.time;
+
+    // Get direction vector towards the mouse cursor
+    Vector2 direction = (mousePos - rb.position).normalized;
+
+    isDashing = true;
+    
+    while (Time.time < startTime + dashTime)
+    {
+        rb.velocity = direction * dashSpeed;
+        yield return null; 
+    }
+
+    isDashing = false;
+}
+
 }
